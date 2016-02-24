bd146f484826255d7090b4486e206b4999773f9a
Валерий Лазакович < >
2016-02-24 19:05:52 +0300

    finished 404 page

--- app/app.js
+++ app/app.js
@@ -11,19 +11,34 @@ angular
     'LocalStorageModule',
     'ui.bootstrap'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider) {
     $routeProvider
+      // .when('/', {
+      //   templateUrl: 'components/tasksComponent/tasksComponent.html',
+      //   controller: 'tasksComponentCtrl'
+      // })
       .when('/', {
+        templateUrl: 'components/fourOhFourComponent/fourOhFour.html'
+      })
+      .when('/tasks', {
         templateUrl: 'components/tasksComponent/tasksComponent.html',
         controller: 'tasksComponentCtrl'
+      })
+      .when('/404', {
+        templateUrl: 'components/fourOhFourComponent/fourOhFour.html'
       })
       // .when('/about', {
       //   templateUrl: 'components/aboutComponent/aboutComponent.html',
       //   controller: 'AboutCtrl'
       // })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/404'
       });
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
   })
   .config(['localStorageServiceProvider', function(localStorageServiceProvider) {
     localStorageServiceProvider.setPrefix('ls');
