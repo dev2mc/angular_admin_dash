16fdee686d51fe382bbaa2768c7854d6d315bac2
Валерий Лазакович < >
2016-03-16 13:20:46 +0300

    finished rewriting unit tests for dashBoardCtrl with using

--- app/components/dashboard/dashBoardCtrl.js
+++ app/components/dashboard/dashBoardCtrl.js
@@ -1,77 +1,71 @@
 'use strict';
-var dashboardCtrl = function (toDoListRemote) {
-  /*jshint validthis:true */
-  var ctrl = this;
-
-  ctrl.stackedCartData = {
-    columns: [
-      ['Sales', 100, 200, 320, 789, 500, 250],
-      ['Users', 50, 325, 125, 450, 233, 350]
-    ],
-    types: {
-      Sales: 'area-spline',
-      Users: 'area-spline'
-    }
-  };
-
-  ctrl.todoListData = [];
-
-  ctrl.getTodoData = function() {
-    toDoListRemote.getAllToDo().then(function(data) {
-      ctrl.todoListData = data;
-    });
-  };
-
-  ctrl.genRandomNumb = function() {
-    var idLength = 7;
-    var numberStr = Math.floor((Math.random() * Math.pow(10, idLength)) + 1) + '';
-    if (numberStr.length < idLength) {
-      var diff = idLength - numberStr.length;
-      for (var i = 1; i <= diff; i++) {
-        numberStr = '0' + numberStr;
-      }
-    }
-    return numberStr;
-  };
-
-  ctrl.changeCompleted = function(id) {
-    angular.forEach(ctrl.todoListData, function(v) {
-      if (v._id === id) {
-        var updItem = angular.copy(v);
-        updItem.completed = !updItem.completed;
-
-        toDoListRemote.updateToDo(updItem, id).then(function() {
-          v.completed = !v.completed;
-        });
-      }
-    });
-  };
-
-  ctrl.delToDoItem = function(id) {
-    angular.forEach(ctrl.todoListData, function(v, i) {
-      if (v._id === id) {
-        toDoListRemote.removeToDo(id).then(function() {
-          ctrl.todoListData.splice(i, 1);
-        });
-      }
-    });
-  };
-
-  ctrl.addToDoItem = function(item) {
-    item._id = ctrl.genRandomNumb();
-    toDoListRemote.addToDo(item).then(function(data) {
-      ctrl.todoListData.push(data);
-    });
-  };
-
-  ctrl.init = function() {
-    ctrl.getTodoData();
-  };
-
-  ctrl.init();
-};
-
-dashboardCtrl.$inject = ['toDoListRemote'];
 
 angular.module('angularDashboardApp')
-  .controller('dashboardCtrl', dashboardCtrl);
+  .controller('dashboardCtrl', function($scope, toDoListRemote) {
+    $scope.stackedCartData = {
+      columns: [
+        ['Sales', 100, 200, 320, 789, 500, 250],
+        ['Users', 50, 325, 125, 450, 233, 350]
+      ],
+      types: {
+        Sales: 'area-spline',
+        Users: 'area-spline'
+      }
+    };
+
+    $scope.todoListData = [];
+
+    $scope.getTodoData = function() {
+      toDoListRemote.getAllToDo().then(function(data) {
+        $scope.todoListData = data;
+      });
+    };
+
+    $scope.genRandomNumb = function() {
+      var idLength = 7;
+      var numberStr = Math.floor((Math.random() * Math.pow(10, idLength)) + 1) + '';
+      if (numberStr.length < idLength) {
+        var diff = idLength - numberStr.length;
+        for (var i = 1; i <= diff; i++) {
+          numberStr = '0' + numberStr;
+        }
+      }
+      return numberStr;
+    };
+
+    $scope.changeCompleted = function(id) {
+      angular.forEach($scope.todoListData, function(v) {
+        if (v._id === id) {
+          var updItem = angular.copy(v);
+          updItem.completed = !updItem.completed;
+
+          toDoListRemote.updateToDo(updItem, id).then(function() {
+            v.completed = !v.completed;
+          });
+        }
+      });
+    };
+
+    $scope.delToDoItem = function(id) {
+      angular.forEach($scope.todoListData, function(v, i) {
+        if (v._id === id) {
+          toDoListRemote.removeToDo(id).then(function() {
+            $scope.todoListData.splice(i, 1);
+          });
+        }
+      });
+    };
+
+    $scope.addToDoItem = function(item) {
+      item._id = $scope.genRandomNumb();
+      toDoListRemote.addToDo(item).then(function(data) {
+        $scope.todoListData.push(data);
+      });
+    };
+
+    $scope.init = function() {
+      $scope.getTodoData();
+    };
+
+    $scope.init();
+  });
--- unit-tests/spec/dashboard/dashBoardCtrlSpec.js
+++ unit-tests/spec/dashboard/dashBoardCtrlSpec.js
@@ -1,7 +1,9 @@
 'use strict';
 
 describe('dashboard: controller: dashBoardCtrl - ', function () {
-  var dashCtrl, toDoListRemote, dummyToDoData;
+  var toDoListRemote, dummyToDoData, scope;
+
+  beforeEach(module('angularDashboardApp'));
 
   beforeEach(function () {
     dummyToDoData = [
@@ -28,40 +30,37 @@ describe('dashboard: controller: dashBoardCtrl - ', function () {
     ];
   });
 
-  beforeEach(function () {
-    var mockToDoListRemote = {};
-
-    module('angularDashboardApp', function($provide) {
-      $provide.value('toDolistRemote', toDoListRemote);
-    });
+  beforeEach(inject(function($rootScope, $controller) {
+    scope = $rootScope.$new();
+    toDoListRemote = {};
 
     inject(function($q) {
-      mockToDoListRemote.data = dummyToDoData;
+      toDoListRemote.data = dummyToDoData;
 
-      mockToDoListRemote.getAllToDo = function() {
+      toDoListRemote.getAllToDo = function() {
         var defer = $q.defer();
-        var response = mockToDoListRemote.data;
+        var response = toDoListRemote.data;
         defer.resolve(response);
         return defer.promise;
       };
 
-      mockToDoListRemote.addToDo = function(todo) {
+      toDoListRemote.addToDo = function(todo) {
         var defer = $q.defer();
         var response = {};
-        mockToDoListRemote.data.push(todo);
+        toDoListRemote.data.push(todo);
         response = todo;
         defer.resolve(response);
         return defer.promise;
       };
 
-      mockToDoListRemote.removeToDo = function(id) {
+      toDoListRemote.removeToDo = function(id) {
         var todoItem;
         var response = {};
 
         var defer = $q.defer();
-        for (var i = 0; i < mockToDoListRemote.data.lenth - 1; i++) {
-          if (mockToDoListRemote.data[i]._id === id) {
-            todoItem = mockToDoListRemote.data.splice(i, 1);
+        for (var i = 0; i < toDoListRemote.data.lenth - 1; i++) {
+          if (toDoListRemote.data[i]._id === id) {
+            todoItem = toDoListRemote.data.splice(i, 1);
           }
         }
         response = todoItem;
@@ -69,12 +68,12 @@ describe('dashboard: controller: dashBoardCtrl - ', function () {
         return defer.promise;
       };
 
-      mockToDoListRemote.updateToDo = function(data, id) {
+      toDoListRemote.updateToDo = function(data, id) {
         var defer = $q.defer();
         var response = {};
-        for (var i = 0; i < mockToDoListRemote.data.lenth - 1; i++) {
-          if (mockToDoListRemote.data[i]._id === id) {
-            mockToDoListRemote.data[i] = data;
+        for (var i = 0; i < toDoListRemote.data.lenth - 1; i++) {
+          if (toDoListRemote.data[i]._id === id) {
+            toDoListRemote.data[i] = data;
           }
         }
         response = data;
@@ -82,74 +81,86 @@ describe('dashboard: controller: dashBoardCtrl - ', function () {
         return defer.promise;
       };
     });
-  });
 
-  beforeEach(inject(function($controller, _toDoListRemote_) {
-    toDoListRemote = _toDoListRemote_;
-    dashCtrl = $controller('dashboardCtrl', {
+    $controller('dashboardCtrl', {
+      $scope: scope,
       toDoListRemote: toDoListRemote
     });
+    scope.$apply();
   }));
 
   describe('init() function execution: ', function () {
     it('should invoke getTodoData() function', function () {
-      spyOn(dashCtrl, 'getTodoData');
-      dashCtrl.init();
-      expect(dashCtrl.getTodoData).toHaveBeenCalled();
+      spyOn(scope, 'getTodoData');
+      scope.init();
+      expect(scope.getTodoData).toHaveBeenCalled();
     });
   });
 
   it('todoListData variable should be defined on controller and should be an array', function () {
-    expect(dashCtrl.todoListData).toBeDefined();
-    expect(Array.isArray(dashCtrl.todoListData)).toBeTruthy();
+    expect(scope.todoListData).toBeDefined();
+    expect(Array.isArray(scope.todoListData)).toBeTruthy();
   });
 
   it('genRandomNumb() function should return a string with length of 7 characters', function () {
     var str = '';
-    str = dashCtrl.genRandomNumb();
+    str = scope.genRandomNumb();
     expect(str.length).toEqual(7);
   });
 
-  describe('changeCompleted() function execution: ', function () {
-    var middleElemInd, middleElem, middleElemId;
+  describe('scope.changeCompleted() function execution: ', function () {
+    var middleElemInd, middleElem, middleElemId, completedOld;
 
     beforeEach(function () {
-      dashCtrl.todoListData = dummyToDoData;
-      middleElemInd = Math.round(dashCtrl.todoListData.length / 2);
-      middleElem = dashCtrl.todoListData[middleElemInd];
+      scope.todoListData = dummyToDoData;
+      middleElemInd = Math.round(scope.todoListData.length / 2);
+      middleElem = scope.todoListData[middleElemInd];
       middleElemId = middleElem._id;
+      completedOld = angular.copy(middleElem.completed);
     });
 
     it('toDoListRemote.updateToDo() function should be invoked with parameters id and todoItem', function () {
       spyOn(toDoListRemote, 'updateToDo').and.callThrough();
-      dashCtrl.changeCompleted(middleElemId);
+      scope.changeCompleted(middleElemId);
 
       var updMiddleElem = angular.copy(middleElem);
       updMiddleElem.completed = !updMiddleElem.completed;
       expect(toDoListRemote.updateToDo).toHaveBeenCalledWith(updMiddleElem, middleElemId);
     });
 
+    it('updated element should have opposite value of completed property', function () {
+      scope.changeCompleted(middleElemId);
+      scope.$apply();
+      var completedNew = middleElem.completed;
+      expect(completedNew).not.toEqual(completedOld);
+    });
   });
 
-  describe('delToDoItem() function execution: ', function () {
+  describe('scope.delToDoItem() function execution: ', function () {
     var middleElemInd, middleElem, middleElemId;
 
     beforeEach(function () {
-      dashCtrl.todoListData = dummyToDoData;
-      middleElemInd = Math.round(dashCtrl.todoListData.length / 2);
-      middleElem = dashCtrl.todoListData[middleElemInd];
+      scope.todoListData = dummyToDoData;
+      middleElemInd = Math.round(scope.todoListData.length / 2);
+      middleElem = scope.todoListData[middleElemInd];
       middleElemId = middleElem._id;
     });
 
-    // dashCtrl.todoListData = dummyToDoData;
-    it('delToDoItem() function should invoke toDoListRemote.removeToDo() function with id parameter', function () {
+    it('scope.delToDoItem() function should invoke toDoListRemote.removeToDo() function with id parameter', function () {
       spyOn(toDoListRemote, 'removeToDo').and.callThrough();
-      dashCtrl.delToDoItem(middleElemId);
+      scope.delToDoItem(middleElemId);
       expect(toDoListRemote.removeToDo).toHaveBeenCalledWith(middleElemId);
+    });
+
+    it('scope.todoListData array should be decreased on one element after scope.delToDoItem() function  invokation', function () {
+      var oldTodoDataLength = scope.todoListData.length;
+      scope.delToDoItem(middleElemId);
+      scope.$apply();
+      expect(oldTodoDataLength - 1).toEqual(scope.todoListData.length);
     });
   });
 
-  describe('addToDoItem() function execution: ', function () {
+  describe('scope.addToDoItem() function execution: ', function () {
     var newItem;
 
     beforeEach(function () {
@@ -161,13 +172,20 @@ describe('dashboard: controller: dashBoardCtrl - ', function () {
 
     it('should invoke toDoListRemote.addToDo() function with parameter item', function () {
       spyOn(toDoListRemote, 'addToDo').and.callThrough();
-      dashCtrl.addToDoItem(newItem);
+      scope.addToDoItem(newItem);
       expect(toDoListRemote.addToDo).toHaveBeenCalledWith(newItem);
     });
 
     it('should add _id property to newItem variable', function () {
-      dashCtrl.addToDoItem(newItem);
+      scope.addToDoItem(newItem);
+      scope.$apply();
       expect(newItem._id).toBeDefined();
+    });
+
+    it('should add newItem to scope.todoListData array', function () {
+      scope.addToDoItem(newItem);
+      scope.$apply();
+      expect(scope.todoListData[scope.todoListData.length - 1]).toEqual(newItem);
     });
   });
 });
