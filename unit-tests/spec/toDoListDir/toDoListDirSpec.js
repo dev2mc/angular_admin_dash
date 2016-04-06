0b6ecc761d62b57749320b5d50ef4577ef744d84
Валерий Лазакович < >
2016-04-06 16:49:22 +0300

    finished dashboard-sales unit tests

--- unit-tests/spec/toDoListDir/toDoListDirSpec.js
+++ unit-tests/spec/toDoListDir/toDoListDirSpec.js
@@ -1,15 +1,9 @@
 'use strict';
 describe('toDoList directive: ', function () {
-  var element, scope, dashboard;
+  var element, scope, ctrlScope, controller, dummyToDoData;
 
-  beforeEach(inject(function($rootScope, $compile) {
-    scope = $rootScope.$new();
-
-    element = '<to-do-list data="dashboard.todoListData" change-completed="dashboard.changeCompleted(id)" del-item="dashboard.delToDoItem(id)" add-item="dashboard.addToDoItem(item)"></to-do-list>';
-
-    dashboard = {};
-
-    dashboard.todoListData = [
+  beforeEach(function () {
+    dummyToDoData = [
       {
         _id: '3937483',
         text: 'Do your ToDo List today at 4AM GMT',
@@ -31,45 +25,142 @@ describe('toDoList directive: ', function () {
         completed: true
       }
     ];
+  });
 
-    dashboard.changeCompleted = function(id) {
-      for (var i = 0; i < dashboard.todoListData.length; i++) {
-        if (id === dashboard.todoListData[i]._id) {
-          dashboard.todoListData[i].completed = !dashboard.todoListData[i].completed;
-        }
-      }
-    };
+  beforeEach(module('angularDashboardApp'));
 
-    dashboard.delToDoItem = function(id) {
-      for (var i = 0; i < dashboard.todoListData.length; i++) {
-        if (id === dashboard.todoListData[i]._id) {
-          dashboard.todoListData.splice(i, 1);
-        }
-      }
-    };
+  beforeEach(module('appTemplates'));
 
-    dashboard.addToDoItem = function(item) {
-      var id = function() {
-        var idLength = 7;
-        var numberStr = Math.floor((Math.random() * Math.pow(10, idLength)) + 1) + '';
-        if (numberStr.length < idLength) {
-          var diff = idLength - numberStr.length;
-          for (var i = 1; i <= diff; i++) {
-            numberStr = '0' + numberStr;
-          }
-        }
-        return numberStr;
-      };
+  beforeEach(inject(function($rootScope, $compile) {
 
-      item._id = id();
+    scope = $rootScope.$new();
 
-      dashboard.todoListData.push(item);
-    };
+    element = '<to-do-list data="todoListData" change-completed="changeCompleted(id)" del-item="delToDoItem(id)" add-item="addToDoItem(item)"></to-do-list>';
 
-    element = $compile(element)(dashboard);
+    scope.todoListData = dummyToDoData;
+    scope.changeCompleted = jasmine.createSpy('changeCompleted');
+    scope.delToDoItem = jasmine.createSpy('delToDoItem');
+    scope.addToDoItem = jasmine.createSpy('addToDoItem');
+
+    element = $compile(element)(scope);
+
+    scope.$apply();
+
+    controller = element.controller('toDoList');
+
+    ctrlScope = element.isolateScope() || element.scope();
   }));
 
-  it("should have an isolated scope with listed properties", function () {
-    var siolated
+  it('should apply template to directive', function () {
+    expect(element.html()).not.toEqual('');
+  });
+
+  it('should have an isolated scope with listed properties', function () {
+    var isolated = element.isolateScope();
+    expect(isolated.data).toEqual(dummyToDoData);
+    expect(isolated.changeCompleted).toBeDefined();
+    expect(isolated.delItem).toBeDefined();
+    expect(isolated.addItem).toBeDefined();
+  });
+
+  describe('directive controller: ', function () {
+    var testNewItem;
+
+    beforeEach(function () {
+      testNewItem = {
+        text: '',
+        completed: false
+      };
+    });
+
+    it('should have addToDoItemVisibility variable with falsy value', function () {
+      expect(ctrlScope.addToDoItemVisibility).toBeFalsy();
+    });
+
+    it('should have dummyNewToDoItem with template object for creating new todo items', function () {
+      expect(ctrlScope.dummyNewToDoItem).toEqual(testNewItem);
+    });
+
+    it('should have a defined newTaskItem', function () {
+      expect(ctrlScope.newToDoItem).toEqual(testNewItem);
+    });
+
+    it('should have function changeAddToDoVisibility that changes the value of addToDoItemVisibility to opposite boolean value', function () {
+      ctrlScope.addToDoItemVisibility = false;
+      ctrlScope.changeAddToDoVisibility();
+      expect(ctrlScope.addToDoItemVisibility).toBeTruthy();
+    });
+
+    it('should have clearNewToDoForm function that resets the value of newToDoItem', function () {
+      ctrlScope.newToDoItem = {
+        text: 'test text',
+        complete: true
+      };
+
+      ctrlScope.clearNewToDoForm();
+      expect(ctrlScope.newToDoItem).toEqual(testNewItem);
+    });
+  });
+
+  describe('compiled elements: ', function () {
+    it('should have amount of todo items equal to amount of objects in toDoData array', function () {
+      var amountToDoElems = element[0].getElementsByClassName('dashboard__todo__item').length;
+
+      var amountToDoObjs = dummyToDoData.length;
+
+      expect(amountToDoElems).toEqual(amountToDoObjs);
+    });
+
+    describe('text of todo items in html: ', function () {
+      var htmlTodoElems, htmlFirstItemText, dotoItemsFirstText, htmlLastItemText, dotoItemsLastText;
+
+      beforeEach(function () {
+        htmlTodoElems = element[0].getElementsByClassName('dashboard__todo__item');
+
+        htmlFirstItemText = htmlTodoElems[0].querySelector('.dashboard__todo__info').querySelector('span').textContent;
+
+        dotoItemsFirstText = dummyToDoData[0].text;
+
+        htmlLastItemText = htmlTodoElems[htmlTodoElems.length - 1].querySelector('.dashboard__todo__info').querySelector('span').textContent;
+
+        dotoItemsLastText = dummyToDoData[dummyToDoData.length - 1].text;
+      });
+
+      it('first elem text in data array should be equal to first elem text of item in html', function () {
+        expect(htmlFirstItemText).toEqual(dotoItemsFirstText);
+      });
+
+      it('last elem text in data array should be equal to last elem text of item in html', function () {
+        expect(htmlLastItemText).toEqual(dotoItemsLastText);
+      });
+    });
+
+    describe('add todo form:', function () {
+      var form;
+
+      beforeEach(function () {
+        form = element.find('form');
+      });
+
+      it('form element should be defined', function () {
+        expect(form).toBeDefined();
+      });
+
+      it('label element should be defined', function () {
+        var labelElem = form.find('label');
+        expect(labelElem).toBeDefined();
+      });
+
+      it('input element should be defined', function () {
+        var inputElem = form.find('label');
+        expect(inputElem).toBeDefined();
+      });
+
+      it('2 button should be defined', function () {
+        var buttons = form.find('button');
+        expect(buttons).toBeDefined();
+        expect(buttons.length).toEqual(2);
+      });
+    });
   });
 });
